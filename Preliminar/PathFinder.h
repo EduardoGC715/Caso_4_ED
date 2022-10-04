6d4154eb8fef6f8df7e1f10bd2e4d349ae857c1f
ggv8 <gabriel.go.v8@gmail.com>
2022-10-03 20:21:28 -0600

    Solución caso preliminar, stacks para navegacion del mapa

--- Preliminar/PathFinder.h
+++ Preliminar/PathFinder.h
@@ -1,10 +1,66 @@
+# pragma once
+# include <iostream>
 # include "../ADT/List.h"
 # include "../ADT/Tree.h"
 # include "Door.h"
 
-void pathFinding() {
-    Stack<TreeNode<Door>>* currentPath = new List<TreeNode<Door>>;
+using namespace std;
+
+template<typename T>
+void printList(List<T>* pList) {
+    cout << "[";
+    for (int index = 0; index < pList->getSize();) {
+        Door* data = pList->get(index++)->data;
+        cout << data->getID();
+        if (index != pList->getSize()) {
+            cout << ", ";
+        }
+    }
+    cout << "]";
+}
+
+template<typename T>
+List<T>* findPending(List<T>* pPathList, List<T>* pClearedList) {
+    List<T>* pendingList = new List<T>;
+    for (int index = 0; index < pPathList->getSize(); ++index) {
+        T* path = pPathList->get(index);
+        if (pClearedList->find(path) == NOT_FOUND) {
+            pendingList->add(path);
+        }
+    }
+    return pendingList;
+}
+
+void pathFinding(Tree<Door>* pDoorMap) {
+    List<TreeNode<Door>>* currentList = new List<TreeNode<Door>>;
+    Stack<TreeNode<Door>>* currentPath = currentList;
     List<TreeNode<Door>>* clearedOptions = new List<TreeNode<Door>>;
+
+    currentPath->push(pDoorMap->getRoot());
+
+    while (clearedOptions->getSize() != pDoorMap->getSize()) {
+        cout << "Current Path: "; printList(currentList); cout << endl;
+        cout << "Cleared Paths: "; printList(clearedOptions); cout << endl;
+
+        List<TreeNode<Door>>* pathList = currentPath->top()->children;
+        cout << "\nOptions: "; printList(pathList); cout << endl;
+        
+        pathList = findPending(pathList, clearedOptions);
+        bool isCleared = pathList->isEmpty();
+        cout << "Is Cleared: " << isCleared << endl;
+
+        if (isCleared) {
+            TreeNode<Door>* discardedPath = currentPath->pop();
+            clearedOptions->add( discardedPath );
+            cout << "Cleared Door #" << discardedPath->data->getID() << endl;
+        } else {
+            int choice = rand() % pathList->getSize();
+            TreeNode<Door>* pathChoice = pathList->get(choice);
+            currentPath->push(pathChoice);
+            cout << "Entered Door #" << pathChoice->data->getID() << endl;
+        }
+        cout << "------------------------------------------------" << endl;
+    }
 
 }
 
--- Preliminar/main.cpp
+++ Preliminar/main.cpp
@@ -4,21 +4,35 @@
 # include "../ADT/List.h"
 # include "../ADT/Tree.h"
 # include "Door.h"
+# include "PathFinder.h"
 
 using namespace std;
 
-void printNodes(TreeNode<Door>* pNode) {
+void printNodes(TreeNode<Door>* pNode, int pRecursionLvl) {
     List<TreeNode<Door>>* list = pNode->children;
     Door* data = pNode->data;
-    cout << data->getID() << "(" << data->hasTunnel() << "):[";
-    for(int i = 0; i < list->getSize(); i++) {
-        printNodes(list->get(i));
+
+    cout << "\n";
+    for (int times = 0; times < pRecursionLvl; ++times) {
+        cout << "    ";
     }
-    cout << "], ";
+
+    cout << data->getID() << ":[";
+    for(int i = 0; i < list->getSize(); i++) {
+        printNodes(list->get(i), pRecursionLvl+1);
+        cout << "\n";
+    }
+
+    if (! list->isEmpty()){
+        for (int times = 0; times < pRecursionLvl; ++times) {
+            cout << "     ";
+        }
+    }
+    cout << "],";
 }
 
 void printTree(Tree<Door>* pTree) {
-    printNodes(pTree->getRoot());
+    printNodes(pTree->getRoot(), 0);
 }
 
 Tree<Door>* generateMap() {
@@ -60,5 +74,16 @@ Tree<Door>* generateMap() {
 int main() {
     Tree<Door>* map = generateMap();
     printTree(map);
+
+
+    short choice;
+    cout << "\nReady? 1/0: ";
+    cin >> choice;
+    if (! choice) {
+        return 0;
+    }
+    cout << "\n----------------- _PathFinder_ -----------------" << endl;
+
+    pathFinding(map);
     return 0;
 }
