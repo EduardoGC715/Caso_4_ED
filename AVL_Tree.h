4bda79aeb8bbd94f7a7d87febc12624655205cbd
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-10-08 13:13:44 -0600

    AVL tree terminado

--- AVL_Tree.h
+++ AVL_Tree.h
@@ -11,22 +11,23 @@ private:
 
 public:
     AVL_Tree(){
-        m_root = NULL;
+        m_root = nullptr;
     }
 
     int max(int t_x, int t_y) {
         return (t_x > t_y) ? t_x : t_y;
     }
     int height(Node<T>* t_node) {
-        if (t_node == NULL)
+        if (t_node == nullptr)
             return 0;
-        return t_node->Get_Height();
+        return t_node->get_height();
     }
     int get_balance_factor(Node<T>* t_node) {
-        if (t_node == NULL)
+        if (t_node == nullptr)
             return 0;
         return height(t_node->get_left()) - height(t_node->get_right());
     }
+
     //standarize
     Node<T>* right_rotate(Node<T>* y) {
         Node<T>* x = y->get_left();
@@ -48,40 +49,61 @@ public:
         return y;
     }
 
-    Node *insertNode(Node<T>* t_node,T* t_data,int t_key) {
-        // Find the correct postion and insert the node
-        if (t_node == NULL)
+    Node<T> *smart_insert_node(T* t_data,int t_key){
+        m_root=insert_node(m_root,t_data,t_key);
+    }
+    Node<T> *insert_node(Node<T>* t_node,T* t_data,int t_key) {
+        if (t_node == nullptr){
             return new Node<T>(t_data,t_key);
+        }
         if (t_key < t_node->get_key()){
-            t_node->set_left(insertNode(t_node->left, t_key));
+            t_node->set_left(insert_node(t_node->get_left(),t_data, t_key));
         }
-        else if (t_key > t_node->get_key())
-            t_node->set_right(insertNode(t_node->right, t_key));
-        else
+        else if (t_key > t_node->get_key()){
+            t_node->set_right(insert_node(t_node->get_right(),t_data, t_key));
+        }
+        else{
             return t_node;
+        }
 
-        // Update the balance factor of each node and
-        // balance the tree
-        node->height = 1 + max(height(node->left),
-                               height(node->right));
-        int balanceFactor = getBalanceFactor(node);
-        if (balanceFactor > 1) {
-            if (key < node->left->key) {
-                return rightRotate(node);
-            } else if (key > node->left->key) {
-                node->left = leftRotate(node->left);
-                return rightRotate(node);
+        t_node->set_height(1 + max(height(t_node->get_left()), height(t_node->get_right())));
+        int balance_factor = get_balance_factor(t_node);
+        if (balance_factor > 1) {
+            if (t_key < t_node->get_left()->get_key()) {
+                return right_rotate(t_node);
+            } else if (t_key > t_node->get_left()->get_key()) {
+                t_node->set_left(left_rotate(t_node->get_left()));
+                return right_rotate(t_node);
             }
         }
-        if (balanceFactor < -1) {
-            if (key > node->right->key) {
-                return leftRotate(node);
-            } else if (key < node->right->key) {
-                node->right = rightRotate(node->right);
-                return leftRotate(node);
+        if (balance_factor < -1) {
+            if (t_key > t_node->get_right()->get_key()) {
+                return left_rotate(t_node);
+            } else if (t_key < t_node->get_right()->get_key()) {
+                t_node->set_right(right_rotate(t_node->get_right()));
+                return left_rotate(t_node);
             }
         }
-        return node;
+        return t_node;
+    }
+
+    void smart_print_tree(std::string indent){
+        print_tree(m_root, indent, true);
+    }
+    void print_tree(Node<T> *root, std::string indent, bool last) {
+        if (root != nullptr) {
+            std::cout << indent;
+            if (last) {
+                std::cout << "R----";
+                indent += "   ";
+            } else {
+                std::cout << "L----";
+                indent += "|  ";
+            }
+            std::cout << root->get_key() << std::endl;
+            print_tree(root->get_left(), indent, false);
+            print_tree(root->get_right(), indent, true);
+        }
     }
 };
 
---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "AVL_Tree.h"
+
+int main() {
+    auto prueba_arbol = new AVL_Tree<int>();
+    prueba_arbol->smart_insert_node(new int(33),33);
+    prueba_arbol->smart_insert_node(new int(13),13);
+    prueba_arbol->smart_insert_node(new int(53),53);
+    prueba_arbol->smart_insert_node(new int(9),9);
+    prueba_arbol->smart_insert_node(new int(21),21);
+    prueba_arbol->smart_insert_node(new int(61),61);
+    prueba_arbol->smart_insert_node(new int(8),8);
+    prueba_arbol->smart_insert_node(new int(11),11);
+
+    prueba_arbol->smart_print_tree("");
+    //root = deleteNode(root, 13);
+    //cout << "After deleting " << endl;
+    //printTree(root, "", true);
+}
