397682b381789c05433b98a1d683d2c4a0d1adb3
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-10-20 16:48:03 -0600

    merge

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-# include <thread>
-# include "Characters/Explorer.h"
-# include "Characters/Freighter.h"
-# include "Characters/Mole.h"
-# include "Threads/GameThread.h"
+#include "Map/Map.h"
 
-# define PLAYERS 2
-# define DURATION 10
-# define MINERS 3
-
+using namespace std;
+//utilizar constantes e implementarlas
 int main() {
-    minerHash* miners = new minerHash;
-    miners->emplace(1, new Explorer);
-    miners->emplace(2, new Freighter);
-    miners->emplace(3, new Mole);
-
-    stratHash* strats = new stratHash;
-
-    GameThread game(PLAYERS, DURATION, MINERS, miners, strats);
-    thread gameThread(ref(game));
-    gameThread.join();
-    
-    delete miners;
-    delete strats;
-    return 0;
+    auto mapa=new Map(15);
+    mapa->generate_map();
+    mapa->print_map();
 }
