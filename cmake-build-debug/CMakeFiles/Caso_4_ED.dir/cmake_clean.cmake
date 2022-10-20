037ab776e30ac82150eeaa7a5302499bf65858d7
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-10-19 19:53:05 -0600

    updates

---
+++ cmake-build-debug/CMakeFiles/Caso_4_ED.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caso_4_ED.dir/Main.cpp.obj"
+  "CMakeFiles/Caso_4_ED.dir/Main.cpp.obj.d"
+  "Caso_4_ED.exe"
+  "Caso_4_ED.exe.manifest"
+  "Caso_4_ED.pdb"
+  "libCaso_4_ED.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Caso_4_ED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
